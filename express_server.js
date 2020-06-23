9fb18ffa52f3ac6b42a36117b420a69a58f8ab6d
Ahmed Alani <s3leha@gmail.com>
2020-06-22 17:48:05 -0700

    new form functionality using post method works, and some code clean up

--- express_server.js
+++ express_server.js
@@ -3,12 +3,25 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 app.get('/', (req, res) => {
   res.send('Hello!!');
@@ -20,14 +33,25 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 app.get('/urls/:shortURL', (req, res) => {
-  const { shortURL } = req.params
-  let templateVars = { shortURL, longURL: urlDatabase.shortURL }
+  const { shortURL } = req.params;
+  const longURL = urlDatabase[shortURL];
+  const templateVars = { shortURL, longURL };
   res.render('urls_show', templateVars);
-})
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const { longURL } = req.body;
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}!`);
